55e8e3521b144d085393cbf8e9f39f254a88cf39
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-08-18 13:13:12 +0300

    change structure classes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-document.querySelector('.popup_button').addEventListener('click', function (e) {
+document.querySelector('.popup__container-button_run').addEventListener('click', function (e) {
    document.querySelector('.popup').classList.remove('show');
-   document.querySelector('.wrapper_circle_button').classList.add('disabled');
+   document.querySelector('.roulette__button').classList.add('disabled');
     turnCircle()
 });
-document.querySelector('.popup_button_stop').addEventListener('click', function (e) {
+document.querySelector('.popup__container-button_stop').addEventListener('click', function (e) {
     document.querySelector('.popup').classList.remove('show');
-    document.querySelector('.wrapper_circle_button').classList.remove('disabled');
-    document.querySelector('.wrapper_circle').style.transform = 'rotate(0deg)'
+    document.querySelector('.roulette__button').classList.remove('disabled');
+    document.querySelector('.roulette__circle').style.transform = 'rotate(0deg)'
 });
-document.querySelector('#circle_button').addEventListener('click', function (e) {
-    document.querySelector('.wrapper_circle_button').classList.add('disabled');
+document.querySelector('.roulette__button-run').addEventListener('click', function (e) {
+    document.querySelector('.roulette__button').classList.add('disabled');
     turnCircle();
 
 });
@@ -27,20 +27,20 @@ function turnCircle(){
                 step++
             }
             angle += step;
-            document.querySelector('.wrapper_circle').style.transform = `rotate(${-angle}deg)`
+            document.querySelector('.roulette__circle').style.transform = `rotate(${-angle}deg)`
         }, 100
     );
     setTimeout(
         () => {
             clearInterval(turn);
             document.querySelector('.popup').classList.add('show');
-            let contentPopup = document.querySelector('.popup_content');
+            let contentPopup = document.querySelector('.popup__container-content_number');
             let offset = 30;
             //This is offset between starting point (beginning of 1) and arrow point
             // Dividing by 60 because we have 6 sections (360/6 = 60)
             // +1 - because we starting count from 1
             let countAngle = (Math.trunc((angle + offset) / 60)) % 6 + 1;
             contentPopup.textContent = countAngle;
-            document.querySelector('.wrapper_circle_button').classList.remove('disabled');
+            document.querySelector('.roulette__button').classList.remove('disabled');
         }, time)
 }
